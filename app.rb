97cf473e69a9b69bfda653470e768c106e8314e6
graphsandstuff <linky@linky.link>
2025-11-19 17:35:16 +0100

    logs

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ prometheus = Prometheus::Client.registry
 
 ports_info = client.get_ports_info()
 ports_info.each_key do |port|
-  puts "Gauging port_#{port} with #{ports_info[port]['rx_packets']}"
+  warn "Gauging port_#{port} with #{ports_info[port]['rx_packets']}"
   prometheus.gauge(
     "port_#{port}_rx_packets".to_sym,
     docstring: "Total number of packets processed by port #{port}",
